d499ae98141b47dc4dd25e42c37b2e9e6dc67c01
Niles Rogoff <nilesrogoff@gmail.com>
2016-06-23 13:53:18 -0400

    New filter

--- create.py
+++ create.py
@@ -13,7 +13,8 @@ flairs = [
     ["No hrkgame", "div.link[data-domain='hrkgame.com']"],
     ["No dlc", ".linkflair-dlc"],
     ["No ended giveaways", ".linkflair-ended"],
-    ["No gleam.io", "div.link[data-domain='gleam.io']"]
+    ["No gleam.io", "div.link[data-domain='gleam.io']"],
+    ["No DLH", "div.link[data-domain='dlh.net']"]
 ]
 combinations = []
 tables = []
